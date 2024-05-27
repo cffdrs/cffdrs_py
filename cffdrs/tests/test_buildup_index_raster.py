4ad99271407fccf949b0edf41de93ace37a486df
Conor Brady <con.brad@gmail.com>
2024-05-27 14:20:48 -0700

    Parametrize zero test cases

--- cffdrs/tests/test_buildup_index_raster.py
+++ cffdrs/tests/test_buildup_index_raster.py
@@ -1,7 +1,15 @@
 
+import pytest
 import numpy as np
 from cffdrs.buildup_index_raster import buildup_index
 
-
-def test_zero_bui_input():
-    assert np.array([0, 0]).all() == buildup_index(np.array([0, 0]), np.array([0, 0])).all()
+@pytest.mark.parametrize(
+    "dc,dmc",
+    [
+        (np.array([0, 0]), np.array([0, 0])),
+        (np.array([0, 100]), np.array([0, 100])),
+        (np.array([-1, 0]), np.array([-1, 0])),
+    ],
+)
+def test_bui_zero_cases(dc, dmc):
+    assert np.array([0, 0]).all() == buildup_index(dc, dmc).all()
