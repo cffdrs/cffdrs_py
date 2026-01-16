b3d84f0ada0ccfebb99ec82f97665e3faadf1310
Brett Edwards <brett.1.edwards@gov.bc.ca>
2026-01-16 12:08:49 -0800

    fmc test

---
+++ cffdrs/tests/test_foliar_moisture_content.py
@@ -0,0 +1,29 @@
+from cffdrs.foliar_moisture_content import foliar_moisture_content
+
+import pytest
+
+
+csv_schema = {
+    "LAT": float,
+    "LONG": float,
+    "ELV": float,
+    "DJ": int,
+    "D0": int,
+    "expected_FoliarMoistureContent": float
+}
+
+def test_foliar_moisture_content(load_csv):
+    test_data = load_csv("cffdrs/tests/data/foliar_moisture_content_data.csv", csv_schema)
+
+    for row in test_data:
+        lat = row["LAT"]
+        long = row["LONG"]
+        elv = row["ELV"]
+        dj = row["DJ"]
+        d0 = row["D0"]
+
+        expected_fmc = row["expected_FoliarMoistureContent"]
+
+        calculated_fmc = foliar_moisture_content(lat, long, elv, dj, d0)
+
+        assert pytest.approx(expected_fmc, 0.01) == calculated_fmc, f"Failed for row: {row} - FMC: {calculated_fmc}"
