ccfcbbed9ef91881fddea0a0785495bd92937851
Brett Edwards <brett.1.edwards@gov.bc.ca>
2026-01-28 15:29:19 -0800

    r behaviour doc string

--- cffdrs/buildup_effect.py
+++ cffdrs/buildup_effect.py
@@ -10,6 +10,8 @@ def buildup_effect(fuel_type: FuelType, bui):
     names are laid out in the same manner as Forestry Canada Fire Danger Group
     (FCFDG)(1992).
 
+    R behavior: if fuel unknown and BUI > 0 -> NA, else -> 1
+
     :param fuel_type: The Fire Behaviour Prediction FuelType
     :param bui: The Buildup Index value
 
@@ -19,7 +21,6 @@ def buildup_effect(fuel_type: FuelType, bui):
     fuel = FUEL_TYPE_DEFAULTS.get(fuel_type)
 
     if fuel is None:
-        # R behavior: if fuel unknown and BUI > 0 -> NA, else -> 1
         return math.nan if bui > 0 else 1.0
 
     buio = fuel["BUIo"]
